73af3866775c6ca95fc9072f5402d85817ebabcf
s20397 <80452936+s20397@users.noreply.github.com>
2021-11-03 19:32:35 +0100

    Update InitialKlasa.java

--- src/InitialKlasa.java
+++ src/InitialKlasa.java
@@ -1,2 +1,22 @@
 public class InitialKlasa {
+    public static void main(String[] args) {
+
+    // create first set
+    Set<Integer> numbers = new HashSet<>();
+    numbers.add(1);
+    numbers.add(2);
+    numbers.add(3);
+    numbers.add(4);
+    System.out.println("Numbers: " + numbers);
+
+    // create second set
+    Set<Integer> primeNumbers = new HashSet<>();
+    primeNumbers.add(2);
+    primeNumbers.add(3);
+    System.out.println("Prime Numbers: " + primeNumbers);
+
+    // Difference between HashSet1 and HashSet2
+    numbers.removeAll(primeNumbers);
+    System.out.println("Numbers without prime numbers: " + numbers);
+    }
 }
